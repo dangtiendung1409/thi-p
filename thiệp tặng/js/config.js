3f0bfaa9fcd778d0e25bf0b631959b6494c8e709
Dũng <114909128+dangtiendung1409@users.noreply.github.com>
2022-12-12 21:41:25 +0700

    Add files via upload

---
+++ thiệp tặng/js/config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ngọc Lan';
+const giftUrl = 'https://pnrtscr.com/kqrkc7link';
+const eventName = 'Chúc Mừng Sinh Nhật ';
+const titleCard = 'Tặng Ngọc Lan';
+const contentCard = 'Chúc chị thêm xinh đẹp , gặp nhiều may mắn trong cuộc sống và kiếm được ny nhó';
+
+const giftImage = 'cj iuuu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
